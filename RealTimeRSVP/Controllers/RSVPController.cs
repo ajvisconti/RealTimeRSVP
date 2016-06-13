fd5f390487108dc457c33b1b04746de7b6b45ba7
Fernando Visca <fvisca@outlook.com>
2016-06-13 12:29:46 +0200

    added RSVPController

---
+++ RealTimeRSVP/Controllers/RSVPController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class RSVPController : Controller
+    {
+        // GET: Reserve
+        public ActionResult Reserve()
+        {
+            return View();
+        }
+
+        // POST: Reserve
+        public ActionResult ReservePost()
+        {
+            return View();
+        }
+
+        // GET: Stats
+        public ActionResult Stats()
+        {
+            return View();
+        }
+
+        // POST: Clear
+        public ActionResult Clear()
+        {
+            return View();
+        }
+    }
+}
